9a0cd168000a567635a0bf7480d254987edf8520
izzatillo <yzokenm@gmail.com>
2025-01-27 13:23:06 +0500

    x-images grid

--- source/Images/Images.js
+++ source/Images/Images.js
@@ -11,148 +11,208 @@ export default class Images extends HTMLElement{
 
 		this.replaceChildren();
 		this.shadow = this.attachShadow({mode: 'closed'});
-
+		
 		DOM: {
+			// Pre define grid class based on the number of images
+			const grid_nxn = `grid-${Math.min(this.#JSON["images"].length, 5)}`;
+
 			let imgs = "";
 			for(const img of this.#JSON["images"]) imgs += `<div><img src="${img}"></div>`;
-
+			
 			this.shadow.innerHTML = `
 				<style>
 					:host{
 						display: inline-block;
-						width: ${this.#JSON["width"]};
 						height: ${this.#JSON["height"]};
-
 						cursor: pointer;
 						user-select: none;
-
 						overflow: hidden;
 					}
-					main{
-						display: inline-block;
-						width: ${this.#JSON["width"]};
-						height: ${this.#JSON["height"]};
 
-						/* Disable scrollbar START */
-						-ms-overflow-style: none; /* IE and Edge */
-						scrollbar-width: none; /* Firefox */
-
-						&::-webkit-scrollbar{
-							display: none;
-						}
-						/* Disable scrollbar END */
-
-						& > :first-child{
-							display: block;
-							width: ${this.#JSON["width"]};
-							height: ${this.#JSON["height"]};
-							transform: scale(1);
-							overflow: hidden;
-
-							&::after{
-								pointer-events: none;
-								content: '${this.#JSON["images"].length}';
-								opacity: 0;
-
-								background-color: rgba(0, 0, 0, 0.7);
-								color: white;
-
-								font-size: 250%;
-
-								width: 100%;
-								height: 100%;
-
-								display: grid;
-								place-items: center;
-
-								position: absolute;
-								top: 0px;
-								left: 0px;
-								z-index: 21;
-
-								transition: opacity ease-in-out 250ms;
-							}
-
-							&:hover{
-								&::after{
-									opacity: 1;
-								}
-							}
-
-
-							& > img{
+					${this.getAttribute("version") === "2" ? 
+						`main{
+							max-width: 800px;
+							width: auto;
+							height: auto;
+							
+							display: grid;
+							grid-template-columns: 1fr 1fr;
+							grid-gap: 3px;
+	
+							& img{
 								width: ${this.#JSON["width"]};
 								height: ${this.#JSON["height"]};
 								object-fit: ${this.#JSON["object-fit"] ?? "initial"};
 							}
-						}
+	
+							&.grid-1 {
+								grid-template-columns: 1fr;
+								grid-template-rows: 1fr;
+							}
+	
+							&.grid-2 {
+								grid-template-columns: 1fr 1fr;
+								grid-template-rows: 1fr;
+							}
+	
+							&.grid-3 {
+								grid-template-columns: 1fr 1fr;
+								grid-template-rows: 1fr;
+	
+								> :nth-child(1) {
+									grid-column: span 2;
+									grid-row: span 1;
+								}
+							}
+	
+							&.grid-4 {
+								grid-template-columns: repeat(3, 1fr);
+								grid-template-rows: 1fr;
+	
+								> :nth-child(1) {
+									grid-column: span 3;
+									grid-row: span 1;
+								}
+							}
+	
+							&.grid-5 {
+								grid-template-columns: 1fr 1fr;
+								grid-template-rows: 1fr 1fr;
+	
+								> :nth-child(1) {
+									grid-column: span 2;
+									grid-row: span 2;
+								}
+							}
+						}`
 
-						& > div{
-							display: none;
-							width: 0px;
-							height: 0px;
-							transform: scale(0);
-						}
+						: 
 
-						&.active{
-							background: hsla(0deg, 0%, 0%, 0.5);
-							backdrop-filter: blur(20px);
-							-webkit-backdrop-filter: blur(20px);
+						`main{
+							display: inline-block;
+							width: ${this.#JSON["width"]};
+							height: ${this.#JSON["height"]};
 
-							height: 100dvh;
-							width: 100dvw;
+							/* Disable scrollbar START */
+							-ms-overflow-style: none; /* IE and Edge */
+							scrollbar-width: none; /* Firefox */
 
-							overflow-y: hidden;
-							overflow-x: scroll;
-							scroll-snap-type: x mandatory;
-
-							display: flex;
-							flex-direction: row;
-
-							position: fixed;
-							top: 0;
-							left: 0;
-							z-index: 20;
+							&::-webkit-scrollbar{
+								display: none;
+							}
+							/* Disable scrollbar END */
 
 							& > :first-child{
+								display: block;
+								width: ${this.#JSON["width"]};
+								height: ${this.#JSON["height"]};
+								transform: scale(1);
+								overflow: hidden;
+
+								&::after{
+									pointer-events: none;
+									content: '${this.#JSON["images"].length}';
+									opacity: 0;
+
+									background-color: rgba(0, 0, 0, 0.7);
+									color: white;
+
+									font-size: 250%;
+
+									width: 100%;
+									height: 100%;
+
+									display: grid;
+									place-items: center;
+
+									position: absolute;
+									top: 0px;
+									left: 0px;
+									z-index: 21;
+
+									transition: opacity ease-in-out 250ms;
+								}
+
 								&:hover{
 									&::after{
-										transition: opacity ease-in-out 0ms;
-										opacity: 0;
+										opacity: 1;
 									}
+								}
+
+
+								& > img{
+									width: ${this.#JSON["width"]};
+									height: ${this.#JSON["height"]};
+									object-fit: ${this.#JSON["object-fit"] ?? "initial"};
 								}
 							}
 
 							& > div{
-								overflow: hidden;
+								display: none;
+								width: 0px;
+								height: 0px;
+								transform: scale(0);
+							}
 
-								transform: unset;
-								width: unset;
-								height: unset;
+							&.active{
+								background: hsla(0deg, 0%, 0%, 0.5);
+								backdrop-filter: blur(20px);
+								-webkit-backdrop-filter: blur(20px);
 
-								min-width: 100dvw;
-								min-height: 100dvh;
+								height: 100dvh;
+								width: 100dvw;
 
-								scroll-snap-align: start;
+								overflow-y: hidden;
+								overflow-x: scroll;
+								scroll-snap-type: x mandatory;
 
-								display: grid;
-								place-content: center;
+								display: flex;
+								flex-direction: row;
 
-								& > img{
-									cursor: initial;
-									width: auto;
-									height: auto;
-									max-width: 90dvw;
-									max-height: 90dvh;
-									object-fit: contain;
-									filter: drop-shadow(0px 5px 20px rgba(0, 0, 0, 0.8));
+								position: fixed;
+								top: 0;
+								left: 0;
+								z-index: 20;
+
+								& > :first-child{
+									&:hover{
+										&::after{
+											transition: opacity ease-in-out 0ms;
+											opacity: 0;
+										}
+									}
+								}
+
+								& > div{
+									overflow: hidden;
+
+									transform: unset;
+									width: unset;
+									height: unset;
+
+									min-width: 100dvw;
+									min-height: 100dvh;
+
+									scroll-snap-align: start;
+
+									display: grid;
+									place-content: center;
+
+									& > img{
+										cursor: initial;
+										width: auto;
+										height: auto;
+										max-width: 90dvw;
+										max-height: 90dvh;
+										object-fit: contain;
+										filter: drop-shadow(0px 5px 20px rgba(0, 0, 0, 0.8));
+									}
 								}
 							}
-						}
+						}`
 					}
 				</style>
-
-				<main>${imgs}</main>
+				<main class="${grid_nxn}">${imgs}</main>
 			`;
 		}
 
