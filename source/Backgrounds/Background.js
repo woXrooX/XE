588fdd1e412d83c8d2fa3907c950d36988619673
woXrooX <woXrooX@gmail.com>
2025-03-20 23:19:37 +0500

    pdate

--- source/Backgrounds/Background.js
+++ source/Backgrounds/Background.js
@@ -2,13 +2,15 @@
 // <x-background>
 // {
 	// "version": 1,
-	// "colors": [],
 	// "parent_selector": "container.x_background_1",
 	// "z_index": -1
 // }
 // </x-background>
 
 export default class Background extends HTMLElement {
+	#canvas = null;
+	#ctx = null;
+
 	#JSON = {};
 
 	constructor() {
@@ -33,46 +35,14 @@ export default class Background extends HTMLElement {
 					filter: blur(40px) contrast(150%);
 					opacity: 0.7;
 				}
-
-				div{
-					position: fixed;
-					top: 0;
-					left: 0;
-					width: 100%;
-					height: 100%;
-					background: linear-gradient(
-						45deg,
-						rgba(0, 255, 100, 0.1),
-						rgba(255, 0, 100, 0.1),
-						rgba(100, 0, 255, 0.1)
-					);
-					mix-blend-mode: overlay;
-					pointer-events: none;
-				}
 			</style>
 			<canvas></canvas>
 			<div></div>
 		`;
 
-
-		this.#init_canvas();
 		this.#style_parent_element();
+		this.#init_canvas();
 		this.#load_version_file();
-	}
-
-	#init_canvas = ()=>{
-		this.canvas = this.shadow.querySelector("canvas");
-		this.ctx = this.canvas.getContext("2d");
-
-		// Set initial canvas size
-		this.canvas.width = window.innerWidth;
-		this.canvas.height = window.innerHeight;
-
-		// Set canvas size on resize
-		window.addEventListener('resize', () => {
-			this.canvas.width = window.innerWidth;
-			this.canvas.height = window.innerHeight;
-		});
 	}
 
 	#style_parent_element = ()=>{
@@ -96,10 +66,25 @@ export default class Background extends HTMLElement {
 		`;
 	}
 
+	#init_canvas = ()=>{
+		this.#canvas = this.shadow.querySelector("canvas");
+		this.#ctx = this.#canvas.getContext("2d");
+
+		// Set initial canvas size
+		this.#canvas.width = window.innerWidth;
+		this.#canvas.height = window.innerHeight;
+
+		// Set canvas size on resize
+		window.addEventListener('resize', () => {
+			this.#canvas.width = window.innerWidth;
+			this.#canvas.height = window.innerHeight;
+		});
+	}
+
 	#load_version_file = async ()=>{
 		try {
 			const module = await import(`./versions/Version_${this.#JSON["version"]}.js`);
-			module.default.init(this.canvas, this.ctx);
+			module.default.init(this.#canvas, this.#ctx);
 		}
 
 		catch (error) {
